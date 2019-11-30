f54a18dee3fe1239c358a38488a66644c34b9c4b
Andrew <aun.sokolov@gmail.com>
2019-11-30 16:11:51 +0300

    Update fast-matrix.lisp

--- fast-matrix.lisp
+++ fast-matrix.lisp
@@ -39,26 +39,25 @@
 
 (defun %build-tree (dimensions)
   "Builds an order tree of multiplications, dynamically solving it for all ranges."
-  (declare (optimize (debug 3) (safety 3)))
   (let* ((n (length dimensions))
          (table (make-array (list n n))))
     (loop :for i :from 0 :below n
           :do (setf (aref table i i) (list i (aref dimensions i) 0)))
     (loop :for i :from 0 :below (1- n)
           :do (setf (aref table i (1+ i))
-                    (cons (list i (1+ i)) (%check-two-matrices (cadr (aref table i i))
-                                                               (cadr (aref table (1+ i) (1+ i)))))))
+                    (cons (list i (1+ i)) (%check-two-matrices (second (aref table i i))
+                                                               (second (aref table (1+ i) (1+ i)))))))
     (loop :for len :from 2 :below n
           :do (loop :for ind :from 0 :below (- n len)
                     :do (setf (aref table ind (+ ind len))
                               (loop :for step :from 1 :upto len
-                                    :with best := (cons (list (car (aref table ind ind))
-                                                              (car (aref table (1+ ind) (+ ind len))))
+                                    :with best := (cons (list (first (aref table ind ind))
+                                                              (first (aref table (1+ ind) (+ ind len))))
                                                         (%add-two-cases
                                                             (cdr (aref table ind ind))
                                                             (cdr (aref table (1+ ind) (+ ind len)))))
-                                    :for next := (cons (list (car (aref table ind (+ ind (1- step))))
-                                                             (car (aref table (+ ind step) (+ ind len))))
+                                    :for next := (cons (list (first (aref table ind (+ ind (1- step))))
+                                                             (first (aref table (+ ind step) (+ ind len))))
                                                        (%add-two-cases
                                                         (cdr (aref table ind (+ ind (1- step))))
                                                         (cdr (aref table (+ ind step) (+ ind len)))))
@@ -66,6 +65,7 @@
                                       :do (setf best next)
                                     :finally (return best)))))
     (aref table 0 (1- n))))
+
 
 
 (defmacro fast-multiply (mult-function &optional extract-dimensions dimensions-vector &rest matrices)
